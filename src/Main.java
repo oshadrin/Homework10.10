c1c615ba4409cfed79a3400ba062976ad9776593
olegshadrin <sholegemail@gmail.com>
2022-10-30 22:58:50 +0300

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+    boolean success=  Data.checkInput("login", "pass123", "pass123");
+    if (success) {
+        System.out.println("Данные введены правильно");
+    } else {
+        System.out.println("Данные введены неверно");
+    }
     }
 }
